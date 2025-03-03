139bc121ea21abc62ed7f63c6200532b975970bb
Carlos Vivanco <carlos.vivanco.08@outlook.com>
2025-03-03 19:56:10 -0300

    Base 1.3

--- front/src/Layout.jsx
+++ front/src/Layout.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "../src/context/AuthContext";
 import { Home } from "../src/pages/Home";
+import { Login } from "../src/pages/Login";
+import { Register } from "../src/pages/Register";
+import { Profile } from "../src/pages/Profile";
+import { SearchEvent } from "../src/pages/SearchEvent";
 
 export const Layout = () => {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="search" element={<SearchEvent />} />
       </Routes>
     </AuthProvider>
   );
