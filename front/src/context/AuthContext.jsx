3e0b79f6e3ea9436a8ed3ca392f7f1a76312971a
Carlos Vivanco <carlos.vivanco.08@outlook.com>
2025-03-03 16:33:05 -0300

    base 1.1

--- front/src/context/AuthContext.jsx
+++ front/src/context/AuthContext.jsx
@@ -2,4 +2,4 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { baseURL } from "../config";
 import { Navigate } from "react-router-dom";
 
-const AuthContext = createContext(null);
+export const AuthContext = createContext(null);
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './index.css'
-import Layout from './Layout'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./index.css";
+import Layout from "./Layout";
 
-
-createRoot(document.getElementById('root')).render(<Layout />)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Layout />
+  </StrictMode>
+);
