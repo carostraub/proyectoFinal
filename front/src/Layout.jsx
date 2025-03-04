eed2ff35313fee30babaa3884b8b8ffd56ea0516
Carlos Vivanco <carlos.vivanco.08@outlook.com>
2025-03-04 12:46:58 -0300

    Base 1.5

--- front/src/Layout.jsx
+++ front/src/Layout.jsx
@@ -6,7 +6,6 @@ import { Login } from "../src/pages/Login";
 import { Register } from "../src/pages/Register";
 import { Profile } from "../src/pages/Profile";
 import { SearchEvent } from "../src/pages/SearchEvent";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Layout = () => {
   return (
